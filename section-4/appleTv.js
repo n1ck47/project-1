00ecd6098f500ec8a3fe899e92ed76ff4ecbbe72
Nikhil Sharma <nj.nikhil.jangra@gmail.com>
2021-05-14 23:05:09 +0530

    init

---
+++ section-4/appleTv.js
@@ -0,0 +1,164 @@
+console.log(data);
+
+bigCards = document.getElementById('big-cards');
+bigCard = document.getElementsByClassName('big-card')[0];
+bigCardHeaderImage = document.getElementsByClassName('big-card-header-image')[0];
+bigCardHeaderText = document.getElementsByClassName('big-card-header-text')[0];
+smallCards = document.getElementsByClassName('small-cards');
+
+navigateLeft = document.getElementsByClassName('navigate-left')[0];
+navigateRight = document.getElementsByClassName('navigate-right')[0];
+
+const root = document.querySelector(':root');
+
+function updateBigCard(index){
+    root.style.setProperty('--big-image-big-card',`url('${data[index].big}')`);
+    root.style.setProperty('--medium-image-big-card',`url('${data[index].medium}')`);
+    root.style.setProperty('--small-image-big-card',`url('${data[index].small}')`);
+
+    bigCardHeaderImage.style.backgroundImage = `url('${data[index].logo}')`
+    bigCardHeaderText.innerHTML = data[index].name;
+}
+
+// Set the default big card image
+let index = 0;
+updateBigCard(index);
+
+
+//Big Card Navigation
+navigateLeft.addEventListener('click',()=>{
+    if(index>0){
+        index -=1;
+        updateBigCard(index);
+    }  
+})
+navigateRight.addEventListener('click',()=>{
+    if(index<data.length-1){
+        index +=1;
+        updateBigCard(index);
+    }  
+})
+
+
+//Traverse through the data and add small cards
+data.forEach((currData,index)=>{
+
+    //Add first row of small cards
+    smallCard = document.createElement('div');
+    smallCard.className = 'small-card';
+    smallCard.style.backgroundImage = `url('${currData.big}')`
+    headerText = document.createElement('div');
+    headerText.className = 'small-card-header-text';
+    headerText.innerHTML = currData.name;
+    smallCard.appendChild(headerText);
+    headerImage = document.createElement('div');
+    headerImage.className = 'small-card-header-image';
+    headerImage.style.backgroundImage = `url('${currData.logo}')`
+    smallCard.appendChild(headerImage);
+    smallCards[0].appendChild(smallCard);
+
+    //Add second row of small cards
+    smallCard = document.createElement('div');
+    smallCard.className = 'small-card';
+    smallCard.style.backgroundImage = `url('${currData.big}')`
+    headerText = document.createElement('div');
+    headerText.className = 'small-card-header-text';
+    headerText.innerHTML = currData.name;
+    smallCard.appendChild(headerText);
+    headerImage = document.createElement('div');
+    headerImage.className = 'small-card-header-image';
+    headerImage.style.backgroundImage = `url('${currData.logo}')`
+    smallCard.appendChild(headerImage);
+    smallCards[1].appendChild(smallCard);
+})
+
+
+
+
+
+function handleInf(event) {
+    if(bigCards.scrollLeft >= bigCards.scrollWidth - bigCards.offsetWidth - 100){
+        bigCards.scrollLeft = 0;
+    }
+    console.log(bigCards.scrollLeft,bigCards.scrollWidth - bigCards.offsetWidth - 100);
+}
+
+//bigCards.addEventListener('scroll',handleInf);
+bigCards.scrollLeft = bigCards.scrollWidth / 2;
+
+
+
+
+
+let temp1 = 3, temp2 = 3;
+
+let add1 = true, add2 = true;
+
+
+smallCards[0].scrollLeft = smallCards[0].scrollWidth / 2;
+smallCards[1].scrollLeft = smallCards[1].scrollWidth / 4;
+
+
+
+// Slide animation for small cards
+setInterval(()=>{
+    // For Small Cards 1st
+    // Start sliding Left
+    if(smallCards[0].scrollLeft >= smallCards[0].scrollWidth - smallCards[0].offsetWidth - 100){
+        add1 = false;
+    }
+
+    // Start sliding Right
+    if(smallCards[0].scrollLeft == 0){
+        add1 = true;
+    }
+
+    
+    if(add1){
+        smallCards[0].scrollLeft += temp1;  //Slide Right
+    }
+    else{
+        smallCards[0].scrollLeft -= temp1;  //Slide Left
+    }
+
+    // For Small Cards 2nd
+
+    // Start sliding Left
+    if(smallCards[1].scrollLeft >= smallCards[1].scrollWidth - smallCards[1].offsetWidth - 10){
+        add2 = false;
+    }
+
+    // Start sliding Right
+    if(smallCards[1].scrollLeft == 0){
+        add2 = true;
+    }
+
+    
+    if(add2){
+        smallCards[1].scrollLeft += temp2;  //Slide Right
+    }
+    else{
+        smallCards[1].scrollLeft -= temp2;  //Slide Left
+    }
+
+},50)
+
+// Change slide animation speed while mouse over
+smallCards[0].addEventListener('mouseover',()=>{
+    temp1 = 1;
+})
+
+smallCards[0].addEventListener('mouseleave',()=>{
+    temp1 = 3;
+})
+
+smallCards[1].addEventListener('mouseover',()=>{
+    temp2 = 1;
+})
+
+smallCards[0].addEventListener('mouseleave',()=>{
+    temp2 = 3;
+})
+
+
+console.log(smallCards);
---
+++ section-4/data.js
@@ -0,0 +1,57 @@
+const data = [
+        {
+        name : 'THE MOSQUITO COAST',
+        big : "https://is1-ssl.mzstatic.com/image/thumb/Features125/v4/93/62/dc/9362dc7e-a81a-204c-97fb-6d2a95dde59f/TU9TMV9IZXJvX0NvYXN0X0NTVy1UVkEtV1cucG5n.png/1920x800sr.jpg",
+        medium : "https://is1-ssl.mzstatic.com/image/thumb/Features125/v4/93/62/dc/9362dc7e-a81a-204c-97fb-6d2a95dde59f/TU9TMV9IZXJvX0NvYXN0X0NTVy1UVkEtV1cucG5n.png/1839x766sr.jpg",
+        small : "https://is3-ssl.mzstatic.com/image/thumb/Features125/v4/8c/d0/61/8cd061ee-dd49-8c57-b42e-f46ddc8da329/TU9TMV9IZXJvX0NvYXN0X0NTVC1UVkEtV1cucG5n.png/735x1590.jpg",
+        logo : "https://is5-ssl.mzstatic.com/image/thumb/06Rr8mT3Jom2mi2QMD5XWw/250x113.png"
+    },
+        {
+        name : 'TRYING',
+        big : "https://is4-ssl.mzstatic.com/image/thumb/Features125/v4/8b/c2/86/8bc28680-01e3-0326-3f27-9a069d4d458f/VFJZMl9UZXJ0aWFyeV9Td2luZ3NfQ1NXLVRWQS1XVy1QcmltYXJ5X1RpdGxlLVNlY29uZGFyeV9UaXRsZS5wbmc.png/1920x800sr.jpg",
+        medium : "https://is4-ssl.mzstatic.com/image/thumb/Features125/v4/8b/c2/86/8bc28680-01e3-0326-3f27-9a069d4d458f/VFJZMl9UZXJ0aWFyeV9Td2luZ3NfQ1NXLVRWQS1XVy1QcmltYXJ5X1RpdGxlLVNlY29uZGFyeV9UaXRsZS5wbmc.png/1839x766sr.jpg",
+        small : "https://is5-ssl.mzstatic.com/image/thumb/Features125/v4/2e/97/d2/2e97d231-4431-8cf4-7561-ee3a0d2c771c/VFJZMl9UZXJ0aWFyeV9Td2luZ3NfQ1NULVRWQS1URVItUHJpbWFyeV9UaXRsZS1TZWNvbmRhcnlfVGl0bGUucG5n.png/735x1590.jpg",
+        logo : "https://is1-ssl.mzstatic.com/image/thumb/wLVmr4n0LxL8yYXgksKc1w/250x68.png"
+    },
+        {   
+            name : "FOR ALL MANKIND",
+            big : "https://is4-ssl.mzstatic.com/image/thumb/Features124/v4/d4/46/96/d4469686-1eca-d7fa-a656-ee40565fb993/RkFNMiAtIFJFU1RSSUNURURfR0VPX0VhcnRoIFJpc2VfQ1NXLVRWQS1XVy5wbmc.png/1920x800sr.jpg",
+            medium : "https://is4-ssl.mzstatic.com/image/thumb/Features124/v4/d4/46/96/d4469686-1eca-d7fa-a656-ee40565fb993/RkFNMiAtIFJFU1RSSUNURURfR0VPX0VhcnRoIFJpc2VfQ1NXLVRWQS1XVy5wbmc.png/1839x766sr.jpg",
+            small : "https://is2-ssl.mzstatic.com/image/thumb/Features124/v4/7d/ea/d5/7dead5e8-1030-84fe-f62c-16872a46fa62/RkFNMiAtIFJFU1RSSUNURURfR0VPX0VhcnRoIFJpc2VfQ1NULVRWQS1XVy5wbmc.png/735x1590.jpg",
+            logo : "https://is2-ssl.mzstatic.com/image/thumb/w40iBgtGChwfMeucrupd8A/794x62.png"
+    },
+        {
+            name : "TED LASSO",
+            big : "https://is1-ssl.mzstatic.com/image/thumb/Features124/v4/9d/fc/61/9dfc61c7-2b5d-18c4-6213-1db292358f8f/Q1NXLVRWQS1XVy1URUQxSU5UTC5wbmc.png/1920x800sr.jpg",
+            medium : "https://is1-ssl.mzstatic.com/image/thumb/Features124/v4/9d/fc/61/9dfc61c7-2b5d-18c4-6213-1db292358f8f/Q1NXLVRWQS1XVy1URUQxSU5UTC5wbmc.png/1839x766sr.jpg",
+            small : "https://is1-ssl.mzstatic.com/image/thumb/Features124/v4/ee/b9/d1/eeb9d14b-41c8-c271-e290-a87fc54b400c/Q1NULVRWQS1XVy1URUQxSU5UTC5wbmc.png/735x1590.jpg",
+            logo : "https://is5-ssl.mzstatic.com/image/thumb/3SpRSjRqwoUN-BEPEuZHjw/589x85.png"
+    },{
+            name : "HOME BEFORE DARK",
+            big : "https://is5-ssl.mzstatic.com/image/thumb/Features123/v4/6b/c2/32/6bc2323c-c61c-598c-664c-cabe058e070e/Q1NXLVdXLVRFUi1Ib21lX0JlZm9yZV9EYXJrLUxhdW5jaC5wbmc.png/1920x800sr.jpg",
+            medium : "https://is5-ssl.mzstatic.com/image/thumb/Features123/v4/6b/c2/32/6bc2323c-c61c-598c-664c-cabe058e070e/Q1NXLVdXLVRFUi1Ib21lX0JlZm9yZV9EYXJrLUxhdW5jaC5wbmc.png/1839x766sr.jpg",
+            small : "https://is5-ssl.mzstatic.com/image/thumb/Features123/v4/69/00/c1/6900c16d-1782-a19f-5722-5a2cba9376a9/Q1NULVdXLVRFUi1Ib21lX0JlZm9yZV9EYXJrLUxhdW5jaC5wbmc.png/735x1590.jpg",
+            logo : "https://is2-ssl.mzstatic.com/image/thumb/Y5y3GVnMiY2phLSaNlpYkg/1894x85.png"
+    },
+        {
+            name : "THE YEAR EARTH CHANGED",
+            big : "https://is4-ssl.mzstatic.com/image/thumb/Features124/v4/dd/92/c2/dd92c212-599b-dc31-b029-ab39eb38f212/WUVBIC0gSGVyb19QZW5ndWluc19DU1ctVFZBLVdXLnBuZw.png/1920x800sr.jpg",
+            medium : "https://is4-ssl.mzstatic.com/image/thumb/Features124/v4/dd/92/c2/dd92c212-599b-dc31-b029-ab39eb38f212/WUVBIC0gSGVyb19QZW5ndWluc19DU1ctVFZBLVdXLnBuZw.png/1839x766sr.jpg",
+            small : "https://is5-ssl.mzstatic.com/image/thumb/Features114/v4/e2/6b/92/e26b926f-fc9a-af26-4d6b-8e2525146760/WUVBIC0gSGVyb19QZW5ndWluc19DU1QtVFZBLVdXLnBuZw.png/735x1590.jpg",
+            logo : "https://is4-ssl.mzstatic.com/image/thumb/TkjjH53rhKQRwQLPJ3wk3w/340x107.png"
+    },
+        {
+            name : "MYTHIC QUEST",
+            big : "https://is3-ssl.mzstatic.com/image/thumb/Features124/v4/bf/67/3f/bf673fd1-c14e-eb1a-dee9-75a34ce1728f/TVFVMl9JYW5BbmRQb3BweV9DU1ctVFZBLVdXLnBuZw.png/1920x800sr.jpg",
+            medium : "https://is3-ssl.mzstatic.com/image/thumb/Features124/v4/bf/67/3f/bf673fd1-c14e-eb1a-dee9-75a34ce1728f/TVFVMl9JYW5BbmRQb3BweV9DU1ctVFZBLVdXLnBuZw.png/1839x766sr.jpg",
+            small : "https://is3-ssl.mzstatic.com/image/thumb/Features124/v4/3c/bd/92/3cbd92eb-10b5-4d2e-0127-cbf697ee6528/TVFVMl9JYW5BbmRQb3BweV9DU1QtVFZBLVdXLnBuZw.png/735x1590.jpg",
+            logo : "https://is4-ssl.mzstatic.com/image/thumb/lPHn1FN9X_Dz-usEftE-Fg/340x62.png"
+    },
+        {
+            name : "USS GREYHOUND",
+            big : "https://is1-ssl.mzstatic.com/image/thumb/Features113/v4/93/1a/20/931a2001-98e5-dbe7-affc-2e24f3e1cb38/Q1NXLVRWQS1XVy1HUllJTlRMLnBuZw.png/1920x800sr.jpg",
+            medium : "https://is1-ssl.mzstatic.com/image/thumb/Features113/v4/93/1a/20/931a2001-98e5-dbe7-affc-2e24f3e1cb38/Q1NXLVRWQS1XVy1HUllJTlRMLnBuZw.png/1839x766sr.jpg",
+            small : "https://is3-ssl.mzstatic.com/image/thumb/Features123/v4/30/f6/1f/30f61f20-d4d1-a603-f99e-425398b161ce/Q1NULVRWQS1XVy1HUllJTlRMLnBuZw.png/735x1590.jpg",
+            logo : "https://is1-ssl.mzstatic.com/image/thumb/ksprVxWFR2zKEMlg-oN4Lg/803x85.png"
+    }
+]
