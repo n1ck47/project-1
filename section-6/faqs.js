bd3c245d88b94f1fdf2ecbdfe24c0fa18a1ca0b7
Nikhil Sharma <nj.nikhil.jangra@gmail.com>
2021-05-15 16:05:49 +0530

    .

--- section-6/faqs.js
+++ section-6/faqs.js
@@ -1,4 +1,13 @@
-const faqs=document.querySelectorAll(".faq");
+// const faqs=document.querySelectorAll(".faq");
+
+// faqs.forEach(faq =>{
+//   faq.addEventListener("click",() =>{
+//     faq.classList.toggle("active");
+//   });
+// });
+
+
+const faqs=document.querySelectorAll(".section-6-faq");
 
 faqs.forEach(faq =>{
   faq.addEventListener("click",() =>{
